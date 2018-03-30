e78a8fac626639f35e32e3bcedbbf8aea2689415
YKIM <caymmi7777@gmail.com>
2018-03-31 00:35:37 +0900

    로그인 처리 및 회원등급별 헤더 메뉴 변경(+ AjaxView, LogOutController 생성)

---
+++ Semi-Project-Namiya/src/com/namiya/controller/reply/CreateReplyController.java
@@ -0,0 +1,25 @@
+package com.namiya.controller.reply;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.namiya.controller.Controller;
+import com.namiya.model.NamiyaAnswerVO;
+import com.namiya.model.NamiyaDAO;
+import com.namiya.model.NamiyaUserVO;
+
+public class CreateReplyController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//답변을 등록시키고 그 글의 번호를 반환받아 보여주는 컨트롤러
+		String pTitle=request.getParameter("");
+		String pContent=request.getParameter("");
+		HttpSession session=request.getSession(false);
+		NamiyaAnswerVO answerVO=new NamiyaAnswerVO(pTitle, pContent);
+		NamiyaDAO.getInstance().createReply(answerVO);
+		return null;
+	}
+
+}
